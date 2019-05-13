d20309a18a655b41b5a74ca68b69e1b26132c6e6
RODRIGO DA SILVA FERREIRA <rodrigo.sferreira21@senacsp.edu.br>
2019-05-13 17:35:55 -0300

    exercicios

---
+++ atividade1.js
@@ -0,0 +1,18 @@
+// QUESTAO 4
+//“Olá "João", analisei suas preferências e estou vendo que sua
+//or preferida é "verde", seu prato preferido é "macarrão" e
+//que você gosta de ouvir "mpb", agora que já sei de tudo isso,
+//selecionar as melhores notícias para você ler hoje!” 
+
+
+let nome = 'joao';
+let cor = 'verde';
+let comida ='macarrao';
+let musica = 'mpb';
+
+console.log(`Olá ${nome}, analisei suas preferências e estou vendo que sua cor preferida é ${cor},
+seu prato preferido e ${comida} e que você gosta de ouvir ${musica}, agora que já sei de tudo isso,
+vou selecionar as melhores notícias para você ler hoje!`)
+
+
+
